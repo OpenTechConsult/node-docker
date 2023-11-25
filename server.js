d10075bcdf9287c14b1b1a7ad34ec2b99f554387
Elom Atsou Agboka <opentech.consult@gmail.com>
2023-11-25 02:13:17 +0000

    Craft a newly created node.js app
    Set up a simple node  application that responde to the GET / by sending a text formated in HTML H2

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h2>Hi there!</h2>');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
