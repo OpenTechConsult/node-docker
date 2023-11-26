0c295f14e86122986abaebb55c2def099ae220d1
Elom Atsou Agboka <opentech.consult@gmail.com>
2023-11-26 01:06:34 +0000

    Use the container for development
    Setting up project so that the changes made in the local application are reflected in the container

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,4 @@ COPY . .
 
 EXPOSE 3000
 
-CMD ["node", "server.js"]
-
+CMD ["npm", "run", "dev"]
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h2>Hi there dumb ass !!!</h2>');
+    res.send('<h2>Hi there dumbo!!</h2>');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
