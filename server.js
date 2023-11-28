a4e311bd029bb6847c87b47cf3d1a67d198510bf
Elom Atsou Agboka <opentech.consult@gmail.com>
2023-11-28 14:45:26 +0000

    Add a mongo container
    Add a Mongo container, install mongoose package and allow the node-app
    to talk to the database.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 
@@ -7,5 +8,11 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('<h2>Hi there dumbo!! blah !!!</h2>');
 });
+
+async function main() {
+  await mongoose.connect('mongodb://sandro:mypassword@mongo:27017/?authSource=admin');
+}
+
+main().then(() => console.log("successfully connected to DB")).catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
